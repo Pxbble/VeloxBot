8df88d0f33f2cc00c7992055362ada5db972392e
Pxbble <36622426+Pxbble@users.noreply.github.com>
2018-03-05 17:42:43 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,7 +72,7 @@ bot.on("message", function(message) {
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
-    const fetched = await message.channel.fetchMessages({count: deleteCount});
+     const fetched = await message.channel.fetchMessages({count: deleteCount});
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
       break;
