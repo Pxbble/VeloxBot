6cf1014a4a1ea5e2eef4042503e02bcb31dde818
Pxbble <36622426+Pxbble@users.noreply.github.com>
2018-03-05 17:22:56 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,6 +69,15 @@ bot.on("message", function(message) {
       case "shadey":
       message.channel.sendMessage("```The Owner and Creator of the VeloxNetwork```")
       break;
+      if(command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+      }
       case "commands":
       message.channel.sendMessage(message.author.toString() + "**, ```To view the commands, Please go into the channel #commands```**")
       break;
